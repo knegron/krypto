9c0e90789b5ab6f03a84f3dadf277fdcb0889d8b
knegron <kevinnegron@hotmail.com>
2022-07-26 20:47:01 -0400

    initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { TransactionProvider } from '/src/context/TransactionContext';
 import './index.css'
-import { TransactionProvider } from './context/TransactionContext';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <TransactionProvider>
